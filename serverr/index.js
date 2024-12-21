d202be3af34e947c936861bf0495003d766c79b3
new <uday45855@gmail.com>
2024-12-21 19:43:47 +0530

    m

--- serverr/index.js
+++ serverr/index.js
@@ -49,7 +49,7 @@ return res.json(data);
 
 const storage=multer.diskStorage({
     destination:(req,file,cb)=>{
-            cb(null,'public/images')},
+            cb(null,path.join(__dirname,'public/images'))},
     filename:(req,file,cb)=>{
         
 cb(null,file.fieldname+`${Date.now()}`+path.extname(file.originalname))
